6d4913ea2b41ffdd0691870cd7a59e5aba56b8e2
A. Reit <a.reit@procitec.de>
2023-08-04 09:16:47 +0200

    Initial fixes to make Spyder work with Qt 6
    - Spyder GUI shows up with PyQt6, PySide6 is broken (once again, MRO
      issues)
    
    - Test suite mostly passes with PyQt6, PySide6 not tried yet
    
    - The changes are backward compatible with Qt 5

--- spyder/widgets/helperwidgets.py
+++ spyder/widgets/helperwidgets.py
@@ -16,9 +16,9 @@ import qtawesome as qta
 import qstylizer.style
 from qtpy import PYQT5
 from qtpy.QtCore import (
-    QEvent, QPoint, QRegExp, QSize, QSortFilterProxyModel, Qt, Signal)
+    QEvent, QPoint, QRegularExpression, QSize, QSortFilterProxyModel, Qt, Signal)
 from qtpy.QtGui import (QAbstractTextDocumentLayout, QColor, QFontMetrics,
-                        QImage, QPainter, QRegExpValidator, QTextDocument,
+                        QImage, QPainter, QRegularExpressionValidator, QTextDocument,
                         QPixmap)
 from qtpy.QtSvg import QSvgRenderer
 from qtpy.QtWidgets import (
@@ -434,8 +434,8 @@ class FinderLineEdit(ClearLineEdit):
 
         if regex_base is not None:
             # Widget setup
-            regex = QRegExp(regex_base + "{100}")
-            self.setValidator(QRegExpValidator(regex))
+            regex = QRegularExpression(regex_base + "{100}")
+            self.setValidator(QRegularExpressionValidator(regex))
 
     def keyPressEvent(self, event):
         """Qt and FilterLineEdit Override."""
